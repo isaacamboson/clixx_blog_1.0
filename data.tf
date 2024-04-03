5f9fa9bf3f84dfe559be3d9a7f9bd196074dc165
Isaac Amboson <isaacamboson@gmail.com>
2024-04-03 03:14:26 -0400

    pushing files

--- data.tf
+++ data.tf
@@ -10,10 +10,10 @@ data "aws_ami" "stack_ami" {
 }
 
 
-data "aws_route53_zone" "stack_isaac_zone" {
-  name         = "stack-isaac.com." # Notice the dot!!!
-  private_zone = false
-}
+# data "aws_route53_zone" "stack_isaac_zone" {
+#   name         = "stack-isaac.com." # Notice the dot!!!
+#   private_zone = false
+# }
 
 
 data "aws_secretsmanager_secret_version" "creds" {
--- main.tf
+++ main.tf
@@ -411,36 +411,36 @@ resource "local_file" "web_key" {
 # Route53
 #--------------------------------------------------------
 
-# route53 for clixx application
-resource "aws_route53_record" "clixx_route53" {
-  zone_id = data.aws_route53_zone.stack_isaac_zone.id
-  name    = "dev.clixx"
-  type    = "A"
-  # ttl     = 5
+# # route53 for clixx application
+# resource "aws_route53_record" "clixx_route53" {
+#   zone_id = data.aws_route53_zone.stack_isaac_zone.id
+#   name    = "dev.clixx"
+#   type    = "A"
+#   # ttl     = 5
 
-  alias {
-    name                   = aws_lb.clixx_lb.dns_name
-    zone_id                = aws_lb.clixx_lb.zone_id
-    evaluate_target_health = true
-  }
+#   alias {
+#     name                   = aws_lb.clixx_lb.dns_name
+#     zone_id                = aws_lb.clixx_lb.zone_id
+#     evaluate_target_health = true
+#   }
 
-  depends_on = [aws_lb.clixx_lb]
-}
+#   depends_on = [aws_lb.clixx_lb]
+# }
 
-# route53 for blog
-resource "aws_route53_record" "blog_route53" {
-  zone_id = data.aws_route53_zone.stack_isaac_zone.id
-  name    = "dev.blog"
-  type    = "A"
-  # ttl     = 5
+# # route53 for blog
+# resource "aws_route53_record" "blog_route53" {
+#   zone_id = data.aws_route53_zone.stack_isaac_zone.id
+#   name    = "dev.blog"
+#   type    = "A"
+#   # ttl     = 5
 
-  alias {
-    name                   = aws_lb.blog_lb.dns_name
-    zone_id                = aws_lb.blog_lb.zone_id
-    evaluate_target_health = true
-  }
+#   alias {
+#     name                   = aws_lb.blog_lb.dns_name
+#     zone_id                = aws_lb.blog_lb.zone_id
+#     evaluate_target_health = true
+#   }
 
-  depends_on = [aws_lb.blog_lb]
-}
+#   depends_on = [aws_lb.blog_lb]
+# }
 
 
