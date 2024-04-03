27b38c4001a41ac0a937a08e95126acb28b6b88a
Isaac Amboson <isaacamboson@gmail.com>
2024-04-02 23:22:34 -0400

    pushing files

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "stackbuckstateisaac-aut"
-    key            = "terraform_blog+clixx_largeVPC.tfstate"
+    key            = "terraform_blog+clixx_largeVPC.tfstate1"
     region         = "us-east-1"
     dynamodb_table = "statelock-tf"
   }
