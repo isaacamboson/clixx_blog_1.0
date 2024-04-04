afbfee64201f3b6d87763d395123e40c6c2998eb
Isaac Amboson <isaacamboson@gmail.com>
2024-04-04 17:43:01 -0400

    pushing files

--- autoscaling_blog_2.tf
+++ autoscaling_blog_2.tf
@@ -104,7 +104,7 @@ resource "aws_autoscaling_group" "blog_app_asg" {
   max_size                  = 6
   min_size                  = 2
   health_check_grace_period = 300
-  health_check_type         = "ELB"
+  health_check_type         = "EC2"
   vpc_zone_identifier       = [aws_subnet.prv_subnet_1.id, aws_subnet.prv_subnet_6.id]
   target_group_arns         = [aws_lb_target_group.blog_lb_target_group.arn]
   default_cooldown          = 300
